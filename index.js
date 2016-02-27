4729ceb638090eca5457601c7bf7f81305b8aef1
karthik Patanjali <karthik.p@nyu.edu>
2016-02-27 14:26:46 -0500

    Search and replace
    Trying search options in twitter

--- index.js
+++ index.js
@@ -3,16 +3,23 @@
 var Twit = require('twit');
 var twitInfo = require('./config.js');
 var twitter = new Twit(twitInfo);
+var searchString = "world";
 
 var tweets;
 
-twitter.get('search/tweets', { q: '#this.mirror', count: 20 }, function(err, data, response) {
+twitter.get('search/tweets', { q: searchString , count: 2, result_type: 'popular', lang: 'en' }, function(err, data, response) {
   tweets = data.statuses;
   for (index in tweets) {
-    console.log(tweets[index].text);
+    console.log(tweets[index].text.replace(RegExp(searchString, "gi"),'Virtual Reality'));
   }
 })
+
+//.replace(RegExp(searchString, "gi"),'Virtual Reality')
 
 // twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
 //   console.log(data);
 // });
+
+// twitter.get('followers/ids', { screen_name: 'mythilivenkat' },  function (err, data, response) {
+//   console.log(data)
+// })
