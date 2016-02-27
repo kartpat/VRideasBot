bd9f2c363adcccea536ae5eef4c029249b09c554
karthik Patanjali <karthik.p@nyu.edu>
2016-02-26 22:40:50 -0500

    trial for searching tweets

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 
 var Twit = require('twit');
 var twitInfo = require('./config.js');
-
 var twitter = new Twit(twitInfo);
 
-twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data);
-});
+var tweets;
+
+twitter.get('search/tweets', { q: '#this.mirror', count: 20 }, function(err, data, response) {
+  tweets = data.statuses;
+  for (index in tweets) {
+    console.log(tweets[index].text);
+  }
+})
+
+// twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data);
+// });
