c63768cfd617778f5ed2700106ba7efa6ccc78a8
karthik Patanjali <karthik.p@nyu.edu>
2016-02-26 22:16:47 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// this is a test bot for accessing twitter.
+
+var Twit = require('twit');
+var twitInfo = require('./config.js');
+
+var twitter = new Twit(twitInfo);
+
+twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data);
+});
